f6bd173eeb4f660df6dbffa29d42c091fcbe0414
winstonlin01 <110456662+winstonlin01@users.noreply.github.com>
2025-11-15 16:26:55 +0800

    Update event type in helloworld command

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class MyPlugin(Star):
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
+    async def helloworld(self, event: AiocqhttpMessageEvent):
         """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
         message_str = event.message_str # 用户发的纯文本消息字符串
