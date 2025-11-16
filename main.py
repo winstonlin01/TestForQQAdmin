83430e9aac00ace7e0e114276c3d47b4b9325ec0
winstonlin01 <110456662+winstonlin01@users.noreply.github.com>
2025-11-16 14:56:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ class MyPlugin(Star):
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("helloworld",priority=5)
-    async def helloworld(self, event: AiocqhttpMessageEvent):
+    async def helloworld(self, event: AiocqhttpMessageEvent,user_id: str | int):
         """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+        group_id = event.get_group_id()
         role=""
         user_name=""
         try:
