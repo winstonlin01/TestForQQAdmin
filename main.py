78ee0d163f78f0c03630097712460ba869b083bb
winstonlin01 <110456662+winstonlin01@users.noreply.github.com>
2025-11-19 08:28:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
+import re
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.platform import AstrBotMessage
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
-from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import (
-    AiocqhttpMessageEvent,
-)
+from astrbot.api.provider import LLMResponse
+from openai.types.chat.chat_completion import ChatCompletion
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+@register("error_filter", "LuffyLSX", "屏蔽机器人的错误信息回复。", "1.0.0")
+class ErrorFilter(Star):
+    def __init__(self, context: Context, config: dict):
         super().__init__(context)
-
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld",priority=5)
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_id=event.get_sender_id()
-        user_name = event.get_sender_name()
-        group_id = event.get_group_id()
-        role=""
-        user_name=""
-        try:
-            info = await event.bot.get_group_member_info(
-            group_id=int(group_id), user_id=int(user_id), no_cache=True
-            )
-            role = info.get("role", "unknown")
-        except Exception:
-            pass
-        yield event.plain_result(f"Hello, {user_name}, 你是{role}!") # 发送一条纯文本消息
-
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        self.config = config
+        self.IsError_filter = self.config.get('IsError_filter', True)
+    
+    @filter.on_decorating_result()
+    async def on_decorating_result(self, event: AstrMessageEvent):
+        result = event.get_result()
+        message_str = result.get_plain_text()
+        if self.IsError_filter:
+            if '对不起，服务出现异常，请稍后再试！' in message_str:
+                logger.info(message_str)
+                event.stop_event() # 停止回复
