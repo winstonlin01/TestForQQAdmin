2d38340a1e41967c18a678065f6703b8bbbbd881
winstonlin01 <110456662+winstonlin01@users.noreply.github.com>
2025-11-19 08:41:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,13 @@ from astrbot.api.platform import AstrBotMessage
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 from astrbot.api.provider import LLMResponse
-from openai.types.chat.chat_completion import ChatCompletion
 
 @register("error_filter", "winstonlin01", "屏蔽机器人的错误信息回复。", "1.0.0")
 class ErrorFilter(Star):
-    def __init__(self, context: Context, config: dict):
-        super().__init__(context)
-        self.config = config
-        self.IsError_filter = self.config.get('IsError_filter', True)
-    
-    @filter.on_decorating_result()
-    async def on_decorating_result(self, event: AstrMessageEvent):
+    @filter.on_llm_response(priority=100000)
+    async def Stop(self, event: AstrMessageEvent):
         result = event.get_result()
         message_str = result.get_plain_text()
-        if self.IsError_filter:
-            if '对不起，服务出现异常，请稍后再试！' in message_str:
-                logger.info(message_str)
-                event.stop_event() # 停止回复
+        if '对不起，服务出现异常，请稍后再试！' in message_str:
+            logger.info(message_str)
+            event.stop_event() # 停止回复
